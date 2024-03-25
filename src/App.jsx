43f2b41bfc07b896d08fdff09e1d236b282036b1
Rick <46992687+rick-77-2006@users.noreply.github.com>
2024-03-25 13:31:14 +0200

    update-react-hw-03

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-// import ContactList from "./components/Contact";
+import ContactList from "./components/ContactList/ContactList";
 import userData from "./users.json";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactForm from "./components/ContactForm/ContactForm";
-import ContactList from "/src/components/Contact";
 import { nanoid } from "nanoid";
 
 function App() {
